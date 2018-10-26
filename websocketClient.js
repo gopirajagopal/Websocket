27dc25932c7b0652e99dba2b7bb6b5f52eeeb674
gopinath Rajagopal <gopinath.rajagopal1@t-mobile.com>
2018-10-27 00:58:59 +0530

    First commit

---
+++ websocketClient.js
@@ -0,0 +1,57 @@
+//require 
+var WebSocket = require("ws");
+var request = require('request');
+
+//constants
+const  wsurl = "wss://va.msg.liveperson.net/ws_api/account/13350576/messaging/consumer?v=3";
+
+//global variables.
+var jwttoken="";
+var CreateResponse={};
+
+//get jwt
+request.post(
+    'https://va.idp.liveperson.net/api/account/13350576/signup',
+    { json: { key: 'value' } },
+    function (error, response, body) {
+       if (!error && response.statusCode == 200) {
+            jwttoken = body.jwt;   
+            
+
+
+          var header = {
+              headers: {
+                  "Authorization" : "JWT " + jwttoken
+              }
+          };
+
+
+
+//create a web socket
+          var ws = new WebSocket(wsurl, header);
+              ws.addEventListener('error', function (err) {
+                console.log("Error Creating Web Socket");
+                console.log('========== EXIT =========');
+              });
+
+//send a message to create a conversationid
+              ws.addEventListener('open', () => {
+                ws.send('{"kind":"req","id":1,"type":"cm.ConsumerRequestConversation"}');
+              });
+              
+//send message
+              ws.addEventListener('message', event => {
+                console.log(`Message from server: ${event.data}`);
+                CreateResponse = JSON.parse(event.data);
+                    ws.send('{"kind":"req","id":2,"type":"ms.PublishEvent","body":{"dialogId":"'+CreateResponse.body.conversationId+'","event":{"type":"ContentEvent","contentType":"text/plain","message":"Test Message"}}}');
+
+              ws.close(); 
+              });
+       }  
+
+ else if (error) {
+             console.error("error creating JWT");
+                 }
+              }
+
+);
